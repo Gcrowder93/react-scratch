509693fc809b0088cc8215a2fe88ccf98d9c28a5
Chase Crowder <gcrowder93@gmail.com>
2021-12-07 15:32:09 -0800

    drop down now works

--- src/App.js
+++ src/App.js
@@ -18,7 +18,12 @@ function App() {
   }, []);
 
   function filterCountries() {
-    return countries.filter((countries) => countries.name.includes(query));
+    return countries.filter((countries) => {
+      return (
+        countries.name.includes(query) &&
+        (countries.continent === continent || continent === 'Choose')
+      );
+    });
   }
   return (
     <section className="main">
@@ -34,6 +39,12 @@ function App() {
       <select value={continent} onChange={(e) => setContinent(e.target.value)}>
         <option value="Choose">Choose</option>
         <option value="Africa">Africa</option>
+        <option value="Asia">Asia</option>
+        <option value="Antarctica">Antarctica</option>
+        <option value="Europe">Europe</option>
+        <option value="North America">North America</option>
+        <option value="Oceania">Oceania</option>
+        <option value="South America">South America</option>
       </select>
 
       {filterCountries().map((countries) => (
