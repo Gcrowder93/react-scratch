a71349ac0a29ec1f86b6726a09abc2dce5ee73d4
Chase Crowder <gcrowder93@gmail.com>
2021-12-07 16:34:25 -0800

    finished product git push

--- src/components/CountriesCard.js
+++ src/components/CountriesCard.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './CountriesCard.css';
 // import getCountries from '../services/countries';
 
-export default function CountriesCard({ name }) {
+export default function CountriesCard({ name, iso2 }) {
   return (
     <div className="countries-card">
       <div className="details">
         <div className="name">{name}</div>
       </div>
       <div className="image">
-        {/* <img src={'https://flagcdn.com/20x15/png'} /> */}
-        <img src={'https://flagcdn.com/24x18/${countries.iso2.toLowerCase()}.png'} />
+        <img src={`https://flagcdn.com/16x12/${iso2.toLowerCase()}.png`} />
+        {/* <img src={'https://flagcdn.com/24x18/${countries.iso2.toLowerCase()}.png'} /> */}
       </div>
     </div>
   );
