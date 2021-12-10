762dcd7dbd7f1b3a78382f07a4d0d794077c67b5
Chase Crowder <gcrowder93@gmail.com>
2021-12-10 14:33:47 -0800

    added loader

--- src/App.js
+++ src/App.js
@@ -10,13 +10,18 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [query, setQuery] = useState([]);
   const [continent, setContinent] = useState('Choose');
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const fetchData = async () => {
       const countriesData = await getCountries();
       setCountries(countriesData);
+      setLoading(false);
     };
-    fetchData();
-  }, []);
+    if (loading) {
+      fetchData();
+    }
+  }, [loading]);
 
   const filterCountries = countries.filter(
     (countries) =>
@@ -27,34 +32,44 @@ function App() {
   return (
     <section className="main">
       <div className="App">
-        <div className="row">
-          <div className="col">
-            <Header />
-            <input
-              placeholder="Search A Flag!"
-              type="text"
-              value={query}
-              onChange={(e) => {
-                setQuery(e.target.value);
-              }}
-            />
-          </div>
-        </div>
-        <select className="select" value={continent} onChange={(e) => setContinent(e.target.value)}>
-          <option value="Choose">Choose</option>
-          <option value="Africa">Africa</option>
-          <option value="Asia">Asia</option>
-          <option value="Antarctica">Antarctica</option>
-          <option value="Europe">Europe</option>
-          <option value="North America">North America</option>
-          <option value="Oceania">Oceania</option>
-          <option value="South America">South America</option>
-        </select>
-        <div className="box">
-          {filterCountries.map((countries) => (
-            <CountriesCard key={countries.name} {...countries} />
-          ))}
-        </div>
+        <h1>Country List</h1>
+        {loading && <span className="loader"></span>}
+        {!loading && (
+          <>
+            <div className="row">
+              <div className="col">
+                <Header />
+                <input
+                  placeholder="Search A Flag!"
+                  type="text"
+                  value={query}
+                  onChange={(e) => {
+                    setQuery(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <select
+              className="select"
+              value={continent}
+              onChange={(e) => setContinent(e.target.value)}
+            >
+              <option value="Choose">Choose</option>
+              <option value="Africa">Africa</option>
+              <option value="Asia">Asia</option>
+              <option value="Antarctica">Antarctica</option>
+              <option value="Europe">Europe</option>
+              <option value="North America">North America</option>
+              <option value="Oceania">Oceania</option>
+              <option value="South America">South America</option>
+            </select>
+            <div className="box">
+              {filterCountries.map((countries) => (
+                <CountriesCard key={countries.name} {...countries} />
+              ))}
+            </div>
+          </>
+        )}
       </div>
 
       <Footer />
