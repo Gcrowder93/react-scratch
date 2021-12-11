1d3025e9964fc43ac3462dad9400f51cccc55fb1
Chase Crowder <gcrowder93@gmail.com>
2021-12-10 17:24:08 -0800

    got the a-z to work

--- src/App.js
+++ src/App.js
@@ -5,30 +5,44 @@ import Footer from './components/layout/Footer/Footer';
 import CountriesCard from '../src/components/CountriesCard';
 import { getCountries } from './services/countries';
 import { useState, useEffect } from 'react';
+import { TextField, Select, MenuItem } from '@mui/material';
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [query, setQuery] = useState([]);
   const [continent, setContinent] = useState('Choose');
   const [loading, setLoading] = useState(true);
+  const [order, setOrder] = useState('A-Z');
 
   useEffect(() => {
     const fetchData = async () => {
-      const countriesData = await getCountries();
+      const countriesData = await getCountries(order);
       setCountries(countriesData);
       setLoading(false);
     };
     if (loading) {
       fetchData();
     }
-  }, [loading]);
+  }, [loading, order]);
 
-  const filterCountries = countries.filter(
-    (countries) =>
-      countries.name.toLowerCase().includes(query) ||
-      (countries.name.toUpperCase().includes(query) &&
-        (countries.continent === continent || continent === 'Choose'))
-  );
+  const filterCountries = countries
+    .filter(
+      (countries) =>
+        (countries.name.toLowerCase().includes(query) || countries.name.includes(query)) &&
+        (countries.continent === continent || continent === 'Choose')
+    )
+    .sort((a, b) => {
+      if (order === 'A-Z') {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      } else {
+        if (a.name < b.name) return 1;
+        if (a.name > b.name) return -1;
+        return 0;
+      }
+    });
+
   return (
     <section className="main">
       <div className="App">
@@ -39,7 +53,8 @@ function App() {
             <div className="row">
               <div className="col">
                 <Header />
-                <input
+
+                <TextField
                   placeholder="Search A Flag!"
                   type="text"
                   value={query}
@@ -49,20 +64,25 @@ function App() {
                 />
               </div>
             </div>
-            <select
+            <br></br>
+            <Select
               className="select"
               value={continent}
               onChange={(e) => setContinent(e.target.value)}
             >
-              <option value="Choose">Choose</option>
-              <option value="Africa">Africa</option>
-              <option value="Asia">Asia</option>
-              <option value="Antarctica">Antarctica</option>
-              <option value="Europe">Europe</option>
-              <option value="North America">North America</option>
-              <option value="Oceania">Oceania</option>
-              <option value="South America">South America</option>
-            </select>
+              <MenuItem value="Choose">Choose</MenuItem>
+              <MenuItem value="Africa">Africa</MenuItem>
+              <MenuItem value="Asia">Asia</MenuItem>
+              <MenuItem value="Antarctica">Antarctica</MenuItem>
+              <MenuItem value="Europe">Europe</MenuItem>
+              <MenuItem value="North America">North America</MenuItem>
+              <MenuItem value="Oceania">Oceania</MenuItem>
+              <MenuItem value="South America">South America</MenuItem>
+            </Select>
+            <Select className="dropdown" value={order} onChange={(e) => setOrder(e.target.value)}>
+              <MenuItem value="A-Z">A-Z</MenuItem>
+              <MenuItem value="Z-A">Z-A</MenuItem>
+            </Select>
             <div className="box">
               {filterCountries.map((countries) => (
                 <CountriesCard key={countries.name} {...countries} />
